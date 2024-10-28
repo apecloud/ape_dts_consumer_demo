8b5183f63c804c0ae152f377b337bf4b829d9a36
shicai.xu <chenglingsu@126.com>
2024-10-28 18:14:47 +0800

    update demos

--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-pip install -r /home/root/function/requirements.txt --target=/home/root/python && \
-python3 /home/root/index.py
--- function/function.py
+++ function/function.py
@@ -1,8 +1,4 @@
 import log
 
-def handle(record, config):
-    if config.get("print_record") != None and bool(config.get("print_record")):
-        log.log("received record: " + str(record))
-        log.log("received record: " + str(record), "debug")
-        log.log_position("current position: " + str(record))
-        log.log_commit("commited record: " + str(record))
+def handle(record):
+    log.log("received record: " + str(record))
